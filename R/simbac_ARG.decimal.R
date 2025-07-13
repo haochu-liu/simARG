4bcd20078d216220819080cb40487a7754966ca2
LIU <u2008181@live.warwick.ac.uk>
2025-07-13 15:16:55 +0100

    rho and delta for simbac

--- R/simbac_ARG.decimal.R
+++ R/simbac_ARG.decimal.R
@@ -9,16 +9,15 @@
 #' @param delta numeric; If bacteria = TRUE, delta is the mean of recombinant segment length.
 #' @param node_max numeric; Initial maximal node size (default = 1000).
 #' @param output_eff_R logical; If TRUE, return effective recombination rates.
-#' @param optimise_site logical; If TRUE, optimise coalesced sites.
 #' @param edgemat numeric; If TRUE, return full edge material matrix.
 #' @return A list containing matrices of edges and nodes, and other information about ARG.
 #' @export
 #'
 #' @examples
-#' ARG1 <- simbac_ARG(100L, 0.1, 100L, 5)
-#' ARG2 <- simbac_ARG(5L, 0.1, 10L, 1, output_eff_R = TRUE, optimise_site = TRUE)
-simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
-                       optimise_site=FALSE, edgemat=TRUE) {
+#' ARG1 <- simbac_ARG.decimal(100L, 0.1, 100L, 5)
+#' ARG2 <- simbac_ARG.decimal(5L, 0.1, 10L, 1, output_eff_R = TRUE)
+simbac_ARG.decimal <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
+                               edgemat=TRUE) {
   if (!rlang::is_integer(n, n=1)) {
     cli::cli_abort("`n` must be a single integer!")
   } else if (!rlang::is_integer(L, n=1)) {
@@ -37,12 +36,14 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
   colnames(edge_matrix) <- c("node1", "node2", "length")
   edge_mat_index <- rep(NA, node_max)                 # edge material index
   node_height <- rep(NA, node_max)                    # node height to recent time
-  node_mat <- matrix(NA, nrow=node_max, ncol=L)       # node material
+  node_mat <- matrix(NA, nrow=node_max, ncol=L%/%30+1)# node material
   node_eff_R <- matrix(NA, nrow=node_max, ncol=2)     # node effective R
   colnames(node_eff_R) <- c("effective_R", "clonal")
   node_probstart <- matrix(NA, nrow=node_max, ncol=L) # node recomb starting prob
   node_height[1:n] <- 0                               # initialize nodes height
-  node_mat[1:n, ] <- 1                                # initialize nodes material
+  node_mat_col_last <- as.integer(L %% 30)
+  node_mat[1:n, ] <- as.integer(sum(2^(0:29)))
+  node_mat[1:n, L%/%30+1] <- as.integer(sum(2^(0:(node_mat_col_last-1))))
   node_eff_R[1:n, 1] <- rho * (1-(1-1/delta)^(L-1))   # initialize effective R
   node_eff_R[1:n, 2] <- TRUE
 
@@ -57,11 +58,6 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
   edge_index <- 1L
   node_index <- as.integer(n + 1)
   pool <- as.integer(1:n)
-  if (optimise_site) {
-    include_site <- rep(TRUE, L)
-  } else {
-    include_site <- NULL
-  }
 
   while (k > 1) {
     # sample a new event time
@@ -83,18 +79,15 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
 
       # append root node
       node_height[node_index] <- t_sum
-      node_mat[node_index, ] <- as.integer(node_mat[leaf_node[1], ] |
-                                             node_mat[leaf_node[2], ])
+      node_mat_b1 <- decimal2binary_cpp(node_mat[leaf_node[1], ], 30L, node_mat_col_last)
+      node_mat_b2 <- decimal2binary_cpp(node_mat[leaf_node[2], ], 30L, node_mat_col_last)
+      node_mat_b3 <- node_mat_b1 | node_mat_b2
+      node_mat[node_index, ] <- binary2decimal_cpp(node_mat_b3, 30L, node_mat_col_last)
 
       # update effective R
       node_eff_R[node_index, 2] <- any(as.logical(node_eff_R[leaf_node, 2]))
-      if (optimise_site) {
-        list_eff_R <- effective_R(node_mat[node_index, ], delta, rho,
-                                  node_eff_R[node_index, 2], include_site=include_site)
-      } else {
-        list_eff_R <- effective_R(node_mat[node_index, ], delta, rho,
-                                  node_eff_R[node_index, 2])
-      }
+      list_eff_R <- effective_R(node_mat_b3, delta, rho,
+                                node_eff_R[node_index, 2])
       node_eff_R[node_index, 1] <- list_eff_R$R_eff
       node_probstart[node_index, ] <- list_eff_R$probstartcum
 
@@ -103,37 +96,20 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
       edge_index <- edge_index + 2L
       node_index <- node_index + 1L
       k <- k - 1
-
-      if (optimise_site & any(include_site)) {
-        # check if contain exclude sites
-        include_site_index <- which(include_site)
-        for (i in 1:sum(include_site)) {
-          if (sum(node_mat[pool, include_site_index[i]]) == 1) {
-            include_site[include_site_index[i]] <- FALSE
-            for (j in pool) {
-              list_eff_R <- effective_R(node_mat[j, ], delta, rho,
-                                        node_eff_R[j, 2],
-                                        include_site=include_site)
-              node_eff_R[j, 1] <- list_eff_R$R_eff
-              node_probstart[j, ] <- list_eff_R$probstartcum
-            }
-          }
-        }
-      }
-
     } else {
       # recombination event
       leaf_node <- sample(pool, size=1, replace=FALSE, prob=node_eff_R[pool, 1])
+      node_mat_b1 <- decimal2binary_cpp(node_mat[leaf_node, ], 30L, node_mat_col_last)
 
       # sample recombination segment
       x <- which(runif(1) < node_probstart[leaf_node, ])[1]
       # pmf for conditional geom distribution
-      v_s <- which(node_mat[leaf_node, ] &
-                     (node_mat[leaf_node, ] != c(0, node_mat[leaf_node, ][1:(L-1)])))
+      v_s <- which(node_mat_b1 &
+                   (node_mat_b1 != c(0, node_mat_b1[1:(L-1)])))
       site_index <- which(v_s %in% x)
       if (length(site_index)) {
-        v_e <- which(node_mat[leaf_node, ] &
-                       (node_mat[leaf_node, ] != c(node_mat[leaf_node, ][2:L], 0)))
+        v_e <- which(node_mat_b1 &
+                     (node_mat_b1 != c(node_mat_b1[2:L], 0)))
         v_e <- c(v_e[length(v_e)] - L, v_e)
         s <- min(L - (v_s[site_index] - v_e[site_index]), L - x + 1)
       } else {s <- L - x + 1}
@@ -155,9 +131,13 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
 
       edge_mat_index[c(edge_index, edge_index+1)] <- c(node_index, node_index+1)
 
-      node_mat[c(node_index, node_index+1), ] <- 0
-      node_mat[node_index, x:y] <- node_mat[leaf_node, x:y]
-      node_mat[node_index+1, -(x:y)] <- node_mat[leaf_node, -(x:y)]
+      node_mat_b2 <- rep(FALSE, L) # node_mat[node_index, ]
+      node_mat_b3 <- rep(FALSE, L) # node_mat[node_index+1, ]
+      node_mat_b2[x:y] <- node_mat_b1[x:y]
+      node_mat_b3[-(x:y)] <- node_mat_b1[-(x:y)]
+      # update decimal node matrix
+      node_mat[node_index, ] <- binary2decimal_cpp(node_mat_b2, 30L, node_mat_col_last)
+      node_mat[node_index+1, ] <- binary2decimal_cpp(node_mat_b3, 30L, node_mat_col_last)
 
       # append edges
       edge_matrix[c(edge_index, edge_index+1), 1] <- c(node_index, node_index+1L)
@@ -169,24 +149,14 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
 
       # update effective R
       node_eff_R[node_index, 2] <- FALSE
-      if (optimise_site) {
-        list_eff_R <- effective_R(node_mat[node_index, ], delta, rho,
-                                  node_eff_R[node_index, 2], include_site=include_site)
-      } else {
-        list_eff_R <- effective_R(node_mat[node_index, ], delta, rho,
-                                  node_eff_R[node_index, 2])
-      }
+      list_eff_R <- effective_R(node_mat_b2, delta, rho,
+                                node_eff_R[node_index, 2])
       node_eff_R[node_index, 1] <- list_eff_R$R_eff
       node_probstart[node_index, ] <- list_eff_R$probstartcum
 
       node_eff_R[node_index+1, 2] <- node_eff_R[leaf_node, 2]
-      if (optimise_site) {
-        list_eff_R <- effective_R(node_mat[node_index+1, ], delta, rho,
-                                  node_eff_R[node_index+1, 2], include_site=include_site)
-      } else {
-        list_eff_R <- effective_R(node_mat[node_index+1, ], delta, rho,
-                                  node_eff_R[node_index+1, 2])
-      }
+      list_eff_R <- effective_R(node_mat_b3, delta, rho,
+                                node_eff_R[node_index+1, 2])
       node_eff_R[node_index+1, 1] <- list_eff_R$R_eff
       node_probstart[node_index+1, ] <- list_eff_R$probstartcum
 
@@ -202,7 +172,7 @@ simbac_ARG <- function(n, rho_site, L, delta, node_max=1000, output_eff_R=FALSE,
       edge_matrix <- rbind(edge_matrix, matrix(NA, nrow=node_max, ncol=3))
       edge_mat_index <- c(edge_mat_index, rep(NA, node_max))
       node_height <- c(node_height, rep(NA, node_max))
-      node_mat <- rbind(node_mat, matrix(NA, nrow=node_max, ncol=L))
+      node_mat <- rbind(node_mat, matrix(NA, nrow=node_max, ncol=L%/%30+1))
       node_eff_R <- rbind(node_eff_R, matrix(NA, nrow=node_max, ncol=2))
       node_probstart <- rbind(node_probstart, matrix(NA, nrow=node_max, ncol=L))
       node_max <- 2 * node_max
