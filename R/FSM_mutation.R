96472820a6a752ff06775079b469aece98d8ba6c
LIU <u2008181@live.warwick.ac.uk>
2025-08-07 16:38:55 +0100

    fsm mutation

--- R/FSM_mutation.R
+++ R/FSM_mutation.R
@@ -3,22 +3,27 @@
 #' Add mutations uniformly onto the edges of ARG with infinite site assumption.
 #'
 #' @param ARG A `FSM_ARG` object.
-#' @param theta The mutation rate.
+#' @param theta_site The mutation rate per site.
+#' @param binary boolean; If TRUE, return the incidence matrix of nodes, instead of sequences.
 #' @return Add a mutation matrix and a genotype dataframe to the `FSM_ARG` object.
 #' @export
 #'
 #' @examples
 #' ARG <- FSM_ARG(20L, 1, 100L)
-#' ARG_mutation <- ISM_mutation(ARG, 2)
-FSM_mutation <- function(ARG, theta) {
-  if (!inherits(ARG, "sim_FSM_ARG")) {
-    stop("Object must be of class 'sim_FSM_ARG'")
+#' ARG_mutation <- FSM_mutation(ARG, 2)
+FSM_mutation <- function(ARG, theta_site, binary=FALSE) {
+  if (!inherits(ARG, "FSM_ARG")) {
+    stop("Object must be of class 'FSM_ARG'")
   }
 
+  theta <- theta_site * ARG$L
   l <- sum(ARG$edge[, 3])
   n <- rpois(1, theta*l/2) # num of mutations | l ~ Poisson(theta*l/2)
   ARG$mutation <- matrix(NA, nrow=n, ncol=3)
   colnames(ARG$mutation) <- c("edge_index", "pos", "site")
+  if (binary) {
+    ARG$node_site <- matrix(FALSE, nrow=nrow(ARG$node_mat), ncol=ncol(ARG$node_mat))
+  }
   ARG$node <- data.frame(gene_str=rep("[]", length(ARG$node_height)))
   ARG$node$gene <- list(numeric())
 
@@ -28,7 +33,7 @@ FSM_mutation <- function(ARG, theta) {
   # if there are mutations
   mutate_edge <- sample(1:nrow(ARG$edge), n,
                         replace=TRUE, prob=ARG$edge[, 3])
-  mutate_site <- sample(1:ARG$L, n, replace=TRUE)
+  mutate_site <- sample(1:ncol(ARG$node_mat), n, replace=TRUE)
   keep_mutation <- c()
 
   # ignore mutations not in the edge material
@@ -51,10 +56,10 @@ FSM_mutation <- function(ARG, theta) {
 
   # simulate the mutations at every node
   for (i in nrow(ARG$edge):1) {
-    edge_mutation <- ARG$mutation[ARG$mutation[, 1]==i, 3]
+    edge_mutation <- as.integer(ARG$mutation[ARG$mutation[, 1]==i, 3])
     parent_seq <- ARG$node$gene[[ARG$edge[i, 1]]]
     parent_seq <- c(parent_seq, edge_mutation)
-    for (j in 1:ARG$L) {
+    for (j in 1:ncol(ARG$node_mat)) {
       if (ARG$edge_mat[i, j] == 0) {
         parent_seq <- parent_seq[parent_seq != j]
       }
@@ -63,10 +68,23 @@ FSM_mutation <- function(ARG, theta) {
                                               ARG$node$gene[[ARG$edge[i, 2]]]))
   }
 
-  # convert to string
-  for (i in 1:nrow(ARG$node)) {
-    ARG$node$gene_str[i] <- paste0("[", paste(round(ARG$node$gene[[i]], 3),
-                                              collapse = ", "), "]")
+  if (binary) {
+    # convert to incidence matrix
+    for (i in 1:nrow(ARG$node_mat)) {
+      for (j in 1:ncol(ARG$node_mat)) {
+        num_mutation <- sum(ARG$node$gene[[i]] == j)
+        if (num_mutation %% 2) {
+          ARG$node_site[i, j] <- TRUE
+        }
+      }
+    }
+  } else {
+    # convert to string
+    for (i in 1:nrow(ARG$node_mat)) {
+      ARG$node$gene_str[i] <- paste0("[", paste(round(ARG$node$gene[[i]], 3),
+                                                collapse = ", "), "]")
+    }
   }
+
   return(ARG)
 }
