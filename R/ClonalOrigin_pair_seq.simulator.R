346d300a2405f6a40c31f5ff4119b8b680bf8c00
haochu-liu <liu_haochu@outlook.com>
2026-01-28 11:08:09 +0000

    clonalorigin simulator

--- R/ClonalOrigin_pair_seq.simulator.R
+++ R/ClonalOrigin_pair_seq.simulator.R
@@ -20,6 +20,31 @@
 ClonalOrigin_pair_seq.simulator <- function(tree, rho_site, theta_site,
                                             L, delta, N,
                                             k_vec=c(50L, 200L, 2000L)) {
+  if (max(k_vec) > L) {
+    cli::cli_abort("Site distance cannot be greater than the number of sites!")
+  } else if (!rlang::is_integer(k_vec, n=3)) {
+    cli::cli_abort("`k_vec` must be a vector of three integer values!")
+  }
 
+  s_vec <- rep(NA, 7)
+  tree_width <- tree$n
+  v_s <- rep(NA, N*3)
+  for (j in 1:3) {
+    v_r <- rep(NA, N)
+    v_g3 <- rep(NA, N)
+    for (i in 1:N) {
+      ARG <- ClonalOrigin_pair_seq(tree, rho_site, L, delta, k_vec[j])
+      ARG_mutated <- FSM_mutation(ARG, theta_site, binary=TRUE)
+      mat <- ARG_mutated$node_site[1:tree_width, ]
+
+      v_r[i] <- LD_r(mat)
+      v_g3[i] <- G3_test(mat)
+      v_s[i+(j-1)*N] <- any(as.logical(mat[, 1]))
+    }
+    s_vec[j] <- mean(v_r)
+    s_vec[j+3] <- mean(v_g3)
+  }
+  s_vec[7] <- mean(v_s)
+
+  return(s_vec)
 }
-
