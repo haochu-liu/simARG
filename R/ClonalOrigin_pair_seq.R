c921c784c4a4ac2328e6da32b3611d22b6b5307f
haochu-liu <liu_haochu@outlook.com>
2026-02-11 14:30:30 +0000

    organize code

--- R/ClonalOrigin_pair_seq.R
+++ R/ClonalOrigin_pair_seq.R
@@ -49,15 +49,11 @@ ClonalOrigin_pair_seq <- function(tree, rho_site, L, delta, k) {
       survive_index <- which(recomb_edge[1:n_recomb, 6] == 1)
       delta2 <- sum((1 - 1/delta)^c(0:(k-1)))
       R_new <- rpois(1, rho_site*delta2*tree_length/2)
-      if (length(survive_index) >= 0) {
-        R_old <- rbinom(1, length(survive_index), (1 - 1/delta)^k)
-        if (length(survive_index) == 1) {
-          remain_index <- survive_index
-        } else {
-          remain_index <- sample(survive_index, R_old)
-        }
+      R_old <- rbinom(1, length(survive_index), (1 - 1/delta)^k)
+      if (length(survive_index) == 1) {
+        remain_index <- survive_index
       } else {
-        R_old <- 0
+        remain_index <- sample(survive_index, R_old)
       }
     }
 
@@ -81,7 +77,7 @@ ClonalOrigin_pair_seq <- function(tree, rho_site, L, delta, k) {
         i_above_b <- c(0, t_above_b[t_above_b >= 0])
         i_above_b <- i_above_b[2:length(i_above_b)] - i_above_b[1:(length(i_above_b)-1)]
         cuml_above_b <- cumsum(i_above_b * (1+length(i_above_b)):2)
-        num_lineage <- (1+length(i_above_b)) - length(which(a_rexp[j] > cuml_above_b))
+        num_lineage <- (1+length(i_above_b)) - sum(a_rexp[j] > cuml_above_b)
         if (num_lineage == (1+length(i_above_b))) {
           recomb_edge[n_recomb+j, 4] <- a_rexp[j] / num_lineage + recomb_edge[n_recomb+j, 2]
         } else {
@@ -92,7 +88,7 @@ ClonalOrigin_pair_seq <- function(tree, rho_site, L, delta, k) {
         # simulate a_edge
         if (num_lineage > 1) {
           pool_edge <- which((clonal_node_height[clonal_edge[, 1]] >= recomb_edge[n_recomb+j, 4]) &
-                               (clonal_node_height[clonal_edge[, 2]] < recomb_edge[n_recomb+j, 4]))
+                             (clonal_node_height[clonal_edge[, 2]] <  recomb_edge[n_recomb+j, 4]))
           recomb_edge[n_recomb+j, 3] <- sample(pool_edge, 1, replace=TRUE)
         } else {
           recomb_edge[n_recomb+j, 3] <- 2*n - 1
