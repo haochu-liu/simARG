b5e8ef04ad2dd47f6391e2b5e8691fb89a2be96f
haochu-liu <liu_haochu@outlook.com>
2026-02-26 21:48:48 +0000

    node sites and compute m at sites

--- R/homoplasy_index.R
+++ R/homoplasy_index.R
@@ -19,11 +19,20 @@ homoplasy_index <- function(ARG) {
   n_node <- nrow(ARG$node_mat)
   n_leaf <- ARG$n
   n_site <- ncol(ARG$node_mat)
-  m_vec <- rep(NA, n_site)
+  m_vec <- rep(0, n_site)
   s_vec <- rep(NA, n_site)
 
   for (site_loc in 1:n_site) {
     ARG_site <- local_tree_FSM(ARG, site_loc)
+    node_vec <- sort(unique(as.vector(ARG_site$edge[, 1:2])))
+    node_site_vec <- ARG_site$node_site[node_vec, site_loc]
+
+    # Compute minimum possible changes
+    if (any(node_site_vec[1:n_leaf])) {m_vec[site_loc] <- 1}
+
+    # Compute actual changes
+    s_site <- 0
+
   }
 
 }
